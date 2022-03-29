f270f3ad7e98fe2e2ce773daa58a05aa53766061
tamdn <tamdn@runsystem.net>
2022-03-29 16:23:55 +0700

    change pagination ajax to html

--- src/app/page-paginate/page-paginate.component.ts
+++ src/app/page-paginate/page-paginate.component.ts
@@ -1,6 +1,6 @@
 import { data_table } from './../shared/smart-table-settings';
-import { Component, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-page-paginate',
   templateUrl: './page-paginate.component.html',
@@ -10,23 +10,18 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class PagePaginateComponent implements OnInit {
   dataTable = data_table;
   pageIndex = 1;
+  pages: Array<any> = [];
 
   constructor(
-    private route: ActivatedRoute,
-    private router: Router
+    private route: ActivatedRoute
   ){}
 
   ngOnInit(): void {
-  }
-
-  onChange(event: any) {
-    this.pageIndex = event;
-    this.router.navigate(
-      [], 
-      {
-        relativeTo: this.route,
-        queryParams: { currentPage: event }, 
-        queryParamsHandling: 'merge', // remove to replace all query params by provided
-      });
+    this.pageIndex = parseInt(this.route.snapshot.queryParams['page'] || 1);
+    let pg = 1;
+    for(let i = 1; i <= this.dataTable.length; i+= 5) {
+      this.pages.push({pageNumber: pg, current: this.pageIndex === pg});
+      pg++;
+    }
   }
 }
